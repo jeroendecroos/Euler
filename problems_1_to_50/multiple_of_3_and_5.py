d0a7e004ff1b7ed2077d94feb032844eae6824a7
jdcroos <jd_komoon@hotmail.com>
2016-04-01 08:51:07 -0700

    problem 1: multiple of 3 and 5: add basic unittest

--- problems_1_to_50/multiple_of_3_and_5.py
+++ problems_1_to_50/multiple_of_3_and_5.py
@@ -3,6 +3,7 @@
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 import sys
+import unittest
 
 def multiple_generator(maximum=1000, multiples=[3,5]):
     i = 1
@@ -13,8 +14,8 @@ def multiple_generator(maximum=1000, multiples=[3,5]):
                 break
         i+=1
 
-def find_sum_of_multiples_brute(maximum, multiples):
-    return sum(x for x in multiple_generator(maximum, multiples))
+def find_sum_of_multiples_brute(maximum, a,b):
+    return sum(x for x in multiple_generator(maximum, [a,b]))
 
 def sum_divisible_by(multiple, maximum):
     return sum(x for x in xrange(0,maximum,multiple))
@@ -35,8 +36,24 @@ def find_sum_of_multiples_smart(maximum, a,b):
     a_b_sum = sum_divisible_by_smart(a*b, maximum)
     return a_sum + b_sum - a_b_sum
 
-maximum = int(sys.argv[1])
-#print find_sum_of_multiples_brute(maximum, [3,5])
-#print find_sum_of_multiples_brute2(maximum, 3,5)
-print find_sum_of_multiples_smart(maximum, 3,5)
+
+class TestSumMultiples(unittest.TestCase):
+    def test_brute_force(self):
+        self.assertEqual(find_sum_of_multiples_brute(10, 3,5), 23)
+        self.assertEqual(find_sum_of_multiples_brute(1000, 3,5), 233168)
+    def test_brute_force2(self):
+        self.assertEqual(find_sum_of_multiples_brute2(10, 3,5), 23)
+        self.assertEqual(find_sum_of_multiples_brute2(1000, 3,5), 233168)
+    def test_smart(self):
+        self.assertEqual(find_sum_of_multiples_smart(10, 3,5), 23)
+        self.assertEqual(find_sum_of_multiples_smart(1000, 3,5), 233168)
+
+
+if __name__ == '__main__':
+    maximum = int(sys.argv[1])
+    #print find_sum_of_multiples_brute(maximum, 3,5)
+    #print find_sum_of_multiples_brute2(maximum, 3,5)
+    print find_sum_of_multiples_smart(maximum, 3,5)
+
+
 
