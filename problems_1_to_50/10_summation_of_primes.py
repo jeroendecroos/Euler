de1deb0cbc9485c119000d29b1ef744352f79a72
jdcroos <jd_komoon@hotmail.com>
2016-04-14 09:09:30 -0700

    Problem 10: outline

---
+++ problems_1_to_50/10_summation_of_primes.py
@@ -0,0 +1,52 @@
+"""The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+"""
+import sys
+import unittest
+
+def find_summation_of_primes_brute(max_prime):
+    pass 
+
+def find_summation_of_primes_site(max_prime):
+    pass
+
+
+def find_summation_of_primes(max_prime, algorithm='brute'):
+    if algorithm=='brute':
+        answer = find_summation_of_primes_brute(max_prime)
+    elif algorithm=='site':
+        answer = find_summation_of_primes_site(max_prime)
+    else:
+        raise Exception("Unknown algorithm %s" % algorithm)
+    return answer
+
+class TestSummationOfPrimes(unittest.TestCase):
+    def _test_template_example(self, algorithm):
+        self.assertEqual(find_summation_of_primes(10, algorithm), 17)
+
+    def _test_template_solution(self, algorithm):
+        pass
+#        self.assertEqual(find_summation_of_primes(1000, algorithm), 31875000)
+
+    @unittest.expectedFailure
+    def test_site_example(self):
+        self._test_template_example('site')
+    def test_site_solution(self):
+        self._test_template_solution('site')
+    def test_brute_example(self):
+        self._test_template_example('brute')
+    def test_brute_solution(self):
+        self._test_template_solution('brute')
+
+
+if __name__ == '__main__':
+    number = int(sys.argv[1])
+    if len(sys.argv) < 3:
+        answer = find_summation_of_primes(number)
+    else:
+        answer = find_summation_of_primes(number, sys.argv[2])
+    print 'answer=%r' % answer
+
+
+
