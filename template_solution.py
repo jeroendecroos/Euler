52cac9a5f2d61378a819b04a6fa4136ea9cac9eb
jdcroos <jd_komoon@hotmail.com>
2016-04-15 02:51:42 -0700

    update problem template

--- template_solution.py
+++ template_solution.py
@@ -1,44 +1,51 @@
-"""A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+"""The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-a2 + b2 = c2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
-
-There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-Find the product abc.
+Find the sum of all the primes below two million.
 """
 import sys
 import unittest
-import math
 
+def find_summation_of_primes_brute(max_prime):
+    pass 
 
-
-def find_special_pythagorean_triplet_brute(length_sequence):
+def find_summation_of_primes_site(max_prime):
     pass
 
-def find_special_pythagorean_triplet_site(length_sequence):
-    raise Exception('No site solution provided')
 
-
-def find_special_pythagorean_triplet(length_sequence, algorithm='brute'):
+def find_summation_of_primes(max_prime, algorithm='brute'):
     if algorithm=='brute':
-        answer = find_special_pythagorean_triplet_brute(length_sequence)
+        answer = find_summation_of_primes_brute(max_prime)
     elif algorithm=='site':
-        answer = find_special_pythagorean_triplet_site(length_sequence)
+        answer = find_summation_of_primes_site(max_prime)
     else:
         raise Exception("Unknown algorithm %s" % algorithm)
     return answer
 
-class TestSpecialPythagoreanTriplet(unittest.TestCase):
-    def test_brute_force(self):
-        self.assertEqual(find_special_pythagorean_triplet(12,'brute'), 60)
+class TestSummationOfPrimes(unittest.TestCase):
+    def _test_template_example(self, algorithm):
+        self.assertEqual(find_summation_of_primes(10, algorithm), 17)
+
+    def _test_template_solution(self, algorithm):
+        pass
+#        self.assertEqual(find_summation_of_primes(1000, algorithm), 31875000)
+
+    @unittest.expectedFailure
+    def test_site_example(self):
+        self._test_template_example('site')
+    def test_site_solution(self):
+        self._test_template_solution('site')
+    def test_brute_example(self):
+        self._test_template_example('brute')
+    def test_brute_solution(self):
+        self._test_template_solution('brute')
 
 
 if __name__ == '__main__':
     number = int(sys.argv[1])
     if len(sys.argv) < 3:
-        answer = find_special_pythagorean_triplet(number)
+        answer = find_summation_of_primes(number)
     else:
-        answer = find_special_pythagorean_triplet(number, sys.argv[2])
+        answer = find_summation_of_primes(number, sys.argv[2])
     print 'answer=%r' % answer
 
 
